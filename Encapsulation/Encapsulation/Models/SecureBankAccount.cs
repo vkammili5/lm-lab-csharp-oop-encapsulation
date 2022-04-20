e916a85577543ed10a189516ad5c72535850acd4
Kim Diep <kim.diep.techno@gmail.com>
2022-04-20 13:27:59 +0100

    Adds SecureBankAccount model and example in Program.cs

---
+++ Encapsulation/Encapsulation/Models/SecureBankAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Encapsulation.Models
+{
+    public class SecureBankAccount
+    {
+        private readonly string _accountNumber;
+        private const int REWARD_AMOUNT = 50;
+        private int _accountBalance;
+
+        public SecureBankAccount(string accountNumber)
+        {
+            _accountNumber = accountNumber;
+            _accountBalance = 0;
+        }
+
+        public void DepositAmount(int amount)
+        {
+            _accountBalance += amount;
+            AddReward();
+        }
+
+        public void DisplayAccountBalanceDetails()
+        {
+            Console.WriteLine("Your account balance is " + _accountBalance);
+        }
+
+        public void DebitAmount(int amount)
+        {
+            _accountBalance -= amount;
+        }
+
+        private void AddReward()
+        {
+            _accountBalance += REWARD_AMOUNT;
+        }
+    }
+}
+
--- Encapsulation/Encapsulation/Program.cs
+++ Encapsulation/Encapsulation/Program.cs
@@ -29,3 +29,30 @@ Console.WriteLine("Let's make a lot of money right now! Let's change the account
 myDodgyBankAccount.AccountBalance = 1000000;
 myDodgyBankAccount.GetAccountBalanceDetails();
 Console.WriteLine("😻 Weeeeee!!!!\n");
+
+//SECURE BANK ACCOUNT
+Console.WriteLine("--- SECURE BANK ACCOUNT ---\n");
+Console.WriteLine("Mmm...let's make a secure bank account!");
+SecureBankAccount mySecureBankAccount = new(MY_ACCOUNT_NUMBER);
+
+Console.WriteLine("What is my current balance?");
+mySecureBankAccount.DisplayAccountBalanceDetails();
+
+Console.WriteLine("Let's deposit an amount of 30");
+mySecureBankAccount.DepositAmount(30);
+mySecureBankAccount.DisplayAccountBalanceDetails();
+
+Console.WriteLine("Wow! Looks like we get a reward of 50 when we deposit an amount");
+
+Console.WriteLine("Let's be naughty and add rewards without depositing!");
+Console.WriteLine("Oh no :( It looks like we can't do this - it's too secure!");
+//mySecureBankAccount.AddReward();
+//mySecureBankAccount.AddReward();
+//mySecureBankAccount.AddReward();
+mySecureBankAccount.DisplayAccountBalanceDetails();
+
+Console.WriteLine("Let's make a lot of money right now! Let's change the account balance directly!");
+Console.WriteLine("Oh no :( It looks like we can't do this - it's too secure!");
+//mySecureBankAccount._accountBalance = 1000000;
+mySecureBankAccount.DisplayAccountBalanceDetails();
+Console.WriteLine("😹 Well I guess that's secure!");
